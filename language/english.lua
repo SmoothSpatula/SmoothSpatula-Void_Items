a83cbe406f33b5d7082996da3d0124316cb99ec4
Miguel <39136142+LoveBetween@users.noreply.github.com>
2026-03-19 17:19:50 +0100

    Fixed language

--- language/english.lua
+++ language/english.lua
@@ -1,68 +1,63 @@
 return {
     item = {
-        -- Common
         voidsentFlame = {
-            name        = "Voidsent Flames",
-            pickup      = "Full health enemies also detonate on hit. <r>Corrupts all Will-o'-the-wisps.</c>",
-            description = "Upon hitting an enemy at <y>full health</c>, spawn a <y>lava pillar</c> in a <y>12m</c> </g>(+2.4m per stack)</c> radius for <y>260%</c> <g>(+156% per stack)</c> base damage. <r>Corrupts all Will-o'-the-wisps.</c>",
+            name        = "Voidsent Flame",
+            pickup      = "Full health enemies also detonate on hit. <p>Corrupts all Will-o'-the-wisps.</c>",
+            description = "Upon hitting an enemy at <y>full health</c>, spawn a <y>lava pillar</c> in a <y>12m</c> </g>(+2.4m per stack)</c> radius for <y>260%</c> <g>(+156% per stack)</c> base damage. <p>Corrupts all Will-o'-the-wisps.</c>",
         },
 
         weepingFungus = {
             name        = "Weeping Fungus",
-            pickup      = "Heals while sprinting. <r>Corrupts all Bustling Fungus.</c>",
-            description = "While <y>sprinting</c>, heal for <y>2%</c> <g>(+2% per stack)</c> of your <y>maximum health</c> per second. <r>Corrupts all Bustling Fungus.</c>",
+            pickup      = "Heal while sprinting. <p>Corrupts all Bustling Fungi.</c>",
+            description = "<g>Heals</c> for <g>2%</c> <c_stack>(+2% per stack)</c> of your health every second <b>while sprinting</c>. <p>Corrupts all Bustling Fungi</c>.",
         },
 
         needleTick = {
             name        = "Needletick",
-            pickup      = "Chance to collapse enemies on hit. <r>Corrupts all Tri-Tip Daggers.</c>",
-            description = "<y>10%</c> <g>(+10% per stack)</c> chance to apply <y>Collapse</c> on hit, dealing <y>400%</c> total damage. <r>Corrupts all Tri-Tip Daggers.</c>",
+            pickup      = "Chance to collapse enemies on hit. <p>Corrupts all Tri-Tip Daggers.</c>",
+            description = "<y>10%</c> <c_stack>(+10% per stack)</c> chance to <y>collapse</c> an enemy for <y>400%</c> base damage. <p>Corrupts all Tri-Tip Daggers.</c>",
         },
 
-
-        -- Uncommon
         polylute = {
             name        = "Polylute",
-            pickup      = "Chance to fire lightning for massive damage. <r>Corrupts all Ukuleles.</c>",
-            description = "<y>25%</c> chance to fire lightning that deals <y>60%</c> total damage up to <y>3</c> times <g>(+2 per stack)</c>. <r>Corrupts all Ukuleles.</c>",
+            pickup      = "Chance to repeatedly strike a single enemy with lightning. <p>Corrupts all Ukuleles.</c>",
+            description = "<y>25%</c> chance to fire <y>lightning</c> for <y>60%</c> TOTAL damage up to <y>3</c> <c_stack>(+3 per stack)</c> times. <p>Corrupts all Ukuleles.</c>",
         },
 
         saferSpaces = {
             name        = "Safer Spaces",
-            pickup      = "Blocks incoming damage periodically. <r>Corrupts all Tougher Times.</c>",
-            description = "Gain a <y>100%</c> chance to <y>block</c> incoming damage once every <y>15 seconds</c> <g>(-10% per stack)</c>. <r>Corrupts all Tougher Times.</c>",
+            pickup      = "Block the next source of damage. <p>Corrupts all Tougher Times.</c>",
+            description = "<g>Blocks</c> incoming damage once. Recharges after <b>13.5 seconds</c> <c_stack>(-10% per stack)</c>. <p>Corrupts all Tougher Times.</c>",
         },
 
         plasmaShrimp = {
             name        = "Plasma Shrimp",
-            pickup      = "Firing while shielded launches missiles. <r>Corrupts all AtG Missile Mk. 1.</c>",
-            description = "While you have <y>shield</c>, hitting an enemy fires a missile that deals <y>40%</c> <g>(+40% per stack)</c> TOTAL damage. <r>Corrupts all AtG Missile Mk. 1.</c>",
+            pickup      = "While you have shield, fire missiles on every hit. <p>Corrupts all AtG Missile Mk. 1s.</c>",
+            description = "Gain a <g>shield</c> equal to <g>10%</c> of your maximum health. While you have a <g>shield</c>, hitting an enemy fires a missile that deals <y>40%</c> <c_stack>(+40% per stack)</c> TOTAL damage. <p>Corrupts all AtG Missile Mk. 1s.</c>",
         },
 
         lysateCell = {
             name        = "Lysate Cell",
-            pickup      = "Special skill gains an extra charge. <r>Corrupts all Fuel Cells.</c>",
-            description = "Add <y>+1</c> <g>(+1 per stack)</c> charge of your <y>Special skill</c>. <r>Corrupts all Fuel Cells.</c>",
+            pickup      = "Add an extra charge of your Special skill. <p>Corrupts all Fuel Cells.</c>",
+            description = "Add <b>+1</c> <c_stack>(+1 per stack)</c> charge of your <b>Special skill</c>. <b>Reduces Special skill cooldown</c> by <b>33%</c>. <p>Corrupts all Fuel Cells.</c>",
         },
 
-
-        -- Legendary
         pluripotentLarva = {
             name        = "Pluripotent Larva",
-            pickup      = "Revive with a random item. <r>Corrupts all Dio's Best Friend.</c>",
-            description = "Upon death, this item <y>consumes itself</c> and you are <y>reborn</c> with <y>full health</c> plus <y>3</c> <g>(+3 per stack)</c> random items. <r>Corrupts all Dio's Best Friend.</c>",
+            pickup      = "Get a <p>corrupted</c> extra life. Consumed on use. <p>Corrupts all Dio's Best Friends.</c>",
+            description = "<b>Upon death</c>, this item will be <b>consumed</c> and you will <g>return to life</c> with <g>3 seconds of invulnerability</c>, and all of your items that can be <b>corrupted</c> will be. <p>Corrupts all Dio's Best Friends.</c>",
         },
 
         lostSeersLenses = {
             name        = "Lost Seer's Lenses",
-            pickup      = "Chance to instantly kill enemies. <r>Corrupts all Lens-Maker's Glasses.</c>",
-            description = "<y>0.5%</c> <g>(+0.5% per stack)</c> chance to instantly <y>kill</c> a non-boss enemy. <r>Corrupts all Lens-Maker's Glasses.</c>",
+            pickup      = "Gain a 0.5% chance to instantly kill a non-boss enemy. <p>Corrupts all Lens-Maker's Glasses.</c>",
+            description = "Your attacks have a <y>0.5%</c> <c_stack>(+0.5% per stack)</c> chance to <y>instantly kill</c> a <y>non-Boss enemy</c>. <p>Corrupts all Lens-Maker's Glasses.</c>",
         },
 
         benthicBloom = {
             name        = "Benthic Bloom",
-            pickup      = "Upgrades items at the start of each stage. <r>Corrupts all 57 Leaf Clovers.</c>",
-            description = "At the start of each stage, upgrade <y>3</c> <g>(+3 per stack)</c> random items to items of the next rarity. <r>Corrupts all 57 Leaf Clovers.</c>",
+            pickup      = "Upgrades your items at the start of each stage. <p>Corrupts all 57 Leaf Clovers.</c>",
+            description = "<b>Upgrades 3</c> <c_stack>(+3 per stack)</c> random items to items of the next <b>higher rarity</c> at the <b>start of each stage</c>. <p>Corrupts all 57 Leaf Clovers.</c>",
         },
     }
 }
