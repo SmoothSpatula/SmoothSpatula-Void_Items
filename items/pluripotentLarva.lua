283423bbe7db2d55fe9b8df3ec98a0257e32c167
Miguel <39136142+LoveBetween@users.noreply.github.com>
2026-03-24 20:37:16 +0100

    clean up, preventing shader from crashing hot load

--- items/pluripotentLarva.lua
+++ items/pluripotentLarva.lua
@@ -1,18 +1,20 @@
 -- Ration (Used)
 
 local item = Item.new("pluripotentLarva")
-
+local item_used = Item.new("pluripotentLarvaUsed")
 
 -- ===== Assets =====
 
 local sprite = Sprite.new("item/pluripotentLarva", "~/assets/sprites/items/pluripotentLarva.png", 1, 16, 16)
+local sprite_used = Sprite.new("item/pluripotentLarva", "~/assets/sprites/items/pluripotentLarva.png", 1, 16, 16)
 
 -- ===== Properties =====
 
 item:set_sprite(sprite)
 item:set_tier(ItemTier.find("voidRare"))
+item_used:set_sprite(sprite_used)
 
-local is_reviving = false
+local reviving_count = 0
 local revivingUVs = {}
 --revivingUVs[1] = {500, 500, 1000, 0}
 
@@ -28,8 +30,6 @@ Hook.add_pre(gm.constants.actor_death, function(self, other, result, args)
         self.invincible = 180 
         self:item_take(item)
         --self:item_give(used_item)
-        --gm.__rpc_item_proc_dios_friend_implementation__(self.value)
-        
         
         local cam = Global.view_camera
         local camX, camY = gm.camera_get_view_x(cam), gm.camera_get_view_y(cam)
@@ -46,6 +46,7 @@ Hook.add_pre(gm.constants.actor_death, function(self, other, result, args)
 
         --print(screenX, screenY)
         revivingUVs[self.id] = {screenX, screenY, 60, 1} -- time and scale
+        reviving_count = reviving_count + 1
 
         -- add new animation
 
@@ -77,23 +78,21 @@ end)
 
 -- ===== Shaders =====
 
---local shd_void_explosion = gm.find_shader_by_name("shd_void_explosion")
+local shd_void_explosion = gm.find_shader_by_name("shd_void_explosion")
+if shd_void_explosion > -1 then
+    --gm.shader_replace(path.combine(_ENV["!plugins_mod_folder_path"], "shaders", "void_explosion"), "shd_void_explosion", shd_void_explosion)
+else
+    shd_void_explosion = gm.shader_add(path.combine(_ENV["!plugins_mod_folder_path"], "shaders", "void_explosion"), "shd_void_explosion")
+end
 
-local shd_void_explosion = gm.shader_add(path.combine(_ENV["!plugins_mod_folder_path"], "shaders", "void_explosion"), "shd_void_explosion")
-
-local _uni_uvs = gm.shader_get_uniform(shd_void_explosion, "bulgeUVs")
-local _uni_nb = gm.shader_get_uniform(shd_void_explosion, "numBulges")
+local _uni_uvs = gm.shader_get_uniform(shd_void_explosion, "UVS")
+local _uni_nb = gm.shader_get_uniform(shd_void_explosion, "NUM_UVS")
 
 local my_surface = gm.surface_create(1920, 1080)
 gm.post_code_execute("gml_Object_oInit_Draw_73", function()
-    -- if not time_dilation_flag then
-    --     return
-    -- end
-
-
+    if reviving_count < 1 then return end
     local application_surface = gm.variable_global_get("application_surface")
     if gm.surface_exists(application_surface) then
-        
         local app_surf_w = gm.surface_get_width(application_surface)
         local app_surf_h = gm.surface_get_height(application_surface)
         if not gm.surface_exists(my_surface) then
@@ -103,22 +102,19 @@ gm.post_code_execute("gml_Object_oInit_Draw_73", function()
             my_surface = gm.surface_create(app_surf_w, app_surf_h)
         end
 
-        -- max number of bulges
-        local MAX_BULGES = 10
-
-        -- initialize a numeric array
-        local flat = gm.array_create(MAX_BULGES * 4, 0)  -- fills with 0
+        -- max number of UVs
+        local MAX_UVS = 10
+        local flat = gm.array_create(MAX_UVS * 4, 0)
 
         local count = 0
         for id, b in pairs(revivingUVs) do
-            if count >= MAX_BULGES then break end
+            if count >= MAX_UVS then break end
             gm.array_set(flat, count*4 + 0, b[1] or 0) -- x
             gm.array_set(flat, count*4 + 1, b[2] or 0) -- y
             gm.array_set(flat, count*4 + 2, b[3] or 0) -- time
-            gm.array_set(flat, count*4 + 3, b[4] or 0)-- size
+            gm.array_set(flat, count*4 + 3, b[4] or 0) -- size or padding
             count = count + 1
 
-            -- optionally remove expired bulges
             revivingUVs[id][3] = b[3] - 1
             if b[3] < 0 then
                 revivingUVs[id] = nil
--- shaders/void_explosion.fsh
+++ shaders/void_explosion.fsh
@@ -17,41 +17,32 @@ struct Fragment
     float2 vCoord    : TEXCOORD0;
 };
 
-#define MAX_BULGES 10
-uniform float4 bulgeUVs[MAX_BULGES];
-uniform int numBulges;
-// uniform float ShaderStrength;
-// uniform float OutlineThreshold;
-// uniform float OutlineSmoothness;
-// uniform float OutlineStrength;
-// uniform float3 ShaderAccent;
+#define MAX_UVS 10
+uniform float4 UVS[MAX_UVS];
+uniform int NUM_UVS;
 
-float2 coord_warp(float2 uv, float2 resolution, float4 bulgeUVs[MAX_BULGES], out float mask){
+float2 coord_warp(float2 uv, float2 resolution, float4 UVS[MAX_UVS], out float mask){
     float2 pixel = uv * resolution;
     float radius = 200.0f;
 
-    for (int i = 0; i< numBulges; i++){
-        float2 offset = pixel - bulgeUVs[i].xy; // use .xy
+    for (int i = 0; i< NUM_UVS; i++){
+        float2 offset = pixel - UVS[i].xy;
         float dist = length(offset);
         float localMask = saturate((radius - dist) / radius);
         mask = max(mask, localMask);
-        pixel += offset * localMask * ( - bulgeUVs[i].z / 15);
+        pixel += offset * localMask * ( - UVS[i].z / 15);
     }
     return pixel / resolution;
 }
-
 
 float4 main(Fragment INPUT) : SV_Target0 {
     float2 textureDimensions;
     gm_BaseTextureObject.GetDimensions(textureDimensions.x, textureDimensions.y);
 
     float mask;
-    float2 warpedUV = coord_warp(INPUT.vCoord, textureDimensions, bulgeUVs, mask);
+    float2 warpedUV = coord_warp(INPUT.vCoord, textureDimensions, UVS, mask);
     float4 baseColor = gm_BaseTextureObject.Sample(gm_BaseTexture, warpedUV);
     //float3 purple = float3(0.6f, 0.2f, 0.8f);
     //baseColor.rgb = lerp(baseColor.rgb, purple, mask * 0.4f);
-
-    
-
     return baseColor;
 }
