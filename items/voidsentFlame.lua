603e8a175783b0f0983f043486fc156623e17476
Miguel <39136142+LoveBetween@users.noreply.github.com>
2026-03-26 02:14:09 +0100

    Fixed fire_explosion

--- items/voidsentFlame.lua
+++ items/voidsentFlame.lua
@@ -5,24 +5,22 @@ local item = Item.new("voidsentFlame")
 
 local sprite = Sprite.new("item/voidsentFlame", "~/assets/sprites/items/voidsentFlame.png", 1, 16, 16)
 local sprite_effect = Sprite.new("effect/voidsentFlamesef", "~/assets/sprites/effects/voidFlamesEffect1.png", 9, 32, 32)
-
-
--- https://bdragon1727.itch.io
+-- sprite effect used made by https://bdragon1727.itch.io
 
 -- ===== Properties =====
 
 item:set_sprite(sprite)
 item:set_tier(ItemTier.find("voidUncommon"))
 
-sprite_effect:set_speed(1.5)
+sprite_effect:set_speed(1)
 
 -- ===== Callbacks =====
 
 Callback.add(Callback.ON_HIT_PROC, function(attacker, target, hit_info)
     local count = attacker:item_count(item)
     if count <= 0 or target.hp < target.maxhp then return end
-    local size = 96 + 12*(count-1)
-    attacker:fire_explosion(target.x, target.y, attacker.damage * 1.04 + 1.56*count, size, size, sprite_effect, nil, false)
+    local size = 96 + 16*(count-1)
+    attacker:fire_explosion(target.x, target.y, size, size, attacker.damage * 1.04 + 1.56*count, sprite_effect, nil, false)
 
     -- play animation and sound wooo
 end)
