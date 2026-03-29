b0a93c2a86dc12fa2622ff6725139815bf3a5e31
Klehrik <78520710+Klehrik@users.noreply.github.com>
2026-03-29 00:09:14 -0400

    Some desc wording changes

--- language/english.lua
+++ language/english.lua
@@ -3,25 +3,37 @@ return {
         voidsentFlame = {
             name        = "Voidsent Flame",
             pickup      = "Full health enemies also detonate on hit. <p>Corrupts all Will-o'-the-wisps.</c>",
-            description = "Upon hitting an enemy at <y>full health</c>, spawn a <y>lava pillar</c> in a <y>12m</c> </g>(+2.4m per stack)</c> radius for <y>260%</c> <g>(+156% per stack)</c> base damage. <p>Corrupts all Will-o'-the-wisps.</c>",
+            description = "Upon hitting an enemy at <y>full health</c>, spawn a <y>lava pillar</c> in a <y>12m</c> <c_stack>(+2.4m per stack)</c> radius for <y>260%</c> <g>(+156% per stack) <y>damage</c>. <p>Corrupts all Will-o'-the-wisps.</c>",
+            destination = "",
+            date        = "",
+            story       = "",
         },
 
         weepingFungus = {
             name        = "Weeping Fungus",
-            pickup      = "Heal while sprinting. <p>Corrupts all Bustling Fungi.</c>",
-            description = "<g>Heals</c> for <g>2%</c> <c_stack>(+2% per stack)</c> of your health every second <b>while sprinting</c>. <p>Corrupts all Bustling Fungi</c>.",
+            pickup      = "Heal while moving at full speed. <p>Corrupts all Bustling Fungi.</c>",
+            description = "<g>Heal</c> for <g>2%</c> <c_stack>(+2% per stack) health</c> every second <b>while moving at full speed</c>. <p>Corrupts all Bustling Fungi</c>.",
+            destination = "",
+            date        = "",
+            story       = "",
         },
 
         needletick = {
             name        = "Needletick",
             pickup      = "Chance to collapse enemies on hit. <p>Corrupts all Tri-Tip Daggers.</c>",
-            description = "<y>10%</c> <c_stack>(+10% per stack)</c> chance to <y>collapse</c> an enemy for <y>400%</c> base damage. <p>Corrupts all Tri-Tip Daggers.</c>",
+            description = "<y>10%</c> <c_stack>(+10% per stack)</c> chance to <y>collapse</c> an enemy for <y>400% damage</c>. <p>Corrupts all Tri-Tip Daggers.</c>",
+            destination = "",
+            date        = "",
+            story       = "",
         },
 
         polylute = {
             name        = "Polylute",
             pickup      = "Chance to repeatedly strike a single enemy with lightning. <p>Corrupts all Ukuleles.</c>",
-            description = "<y>25%</c> chance to fire <y>lightning</c> for <y>60%</c> TOTAL damage up to <y>3</c> <c_stack>(+3 per stack)</c> times. <p>Corrupts all Ukuleles.</c>",
+            description = "<y>25%</c> chance to fire <y>lightning</c> for <y>3x60% <c_stack>(+60% per stack)</c> TOTAL damage</c>. <p>Corrupts all Ukuleles.</c>",
+            destination = "",
+            date        = "",
+            story       = "",
         },
 
         saferSpaces = {
@@ -36,31 +48,46 @@ return {
         plasmaShrimp = {
             name        = "Plasma Shrimp",
             pickup      = "While you have shield, fire missiles on every hit. <p>Corrupts all AtG Missile Mk. 1s.</c>",
-            description = "Gain a <g>shield</c> equal to <g>10%</c> of your maximum health. While you have a <g>shield</c>, hitting an enemy fires a missile that deals <y>40%</c> <c_stack>(+40% per stack)</c> TOTAL damage. <p>Corrupts all AtG Missile Mk. 1s.</c>",
+            description = "Gain a <b>10% health shield</c>. While you have shield, hitting an enemy fires a missile that deals <y>40% <c_stack>(+40% per stack) TOTAL damage</c>. <p>Corrupts all AtG Missile Mk. 1s.</c>",
+            destination = "",
+            date        = "",
+            story       = "",
         },
 
         lysateCell = {
             name        = "Lysate Cell",
             pickup      = "Add an extra charge of your Special skill. <p>Corrupts all Fuel Cells.</c>",
-            description = "Add <b>+1</c> <c_stack>(+1 per stack)</c> charge of your <b>Special skill</c>. <b>Reduces Special skill cooldown</c> by <b>33%</c>. <p>Corrupts all Fuel Cells.</c>",
+            description = "Add <b>+1 <c_stack>(+1 per stack)</c> charge of your <b>Special skill</c>. <b>Reduces Special skill cooldown</c> by <b>33%</c>. <p>Corrupts all Fuel Cells.</c>",
+            destination = "",
+            date        = "",
+            story       = "",
         },
 
         pluripotentLarva = {
             name        = "Pluripotent Larva",
             pickup      = "Get a <p>corrupted</c> extra life. Consumed on use. <p>Corrupts all Dio's Best Friends.</c>",
-            description = "<b>Upon death</c>, this item will be <b>consumed</c> and you will <g>return to life</c> with <g>3 seconds of invulnerability</c>, and all of your items that can be <b>corrupted</c> will be. <p>Corrupts all Dio's Best Friends.</c>",
+            description = "Taking <r>fatal damage</c> <b>consumes this item</c> and revives you with <g>3 seconds of invulnerability</c>, and all of your items that can be <b>corrupted</c> will be. <p>Corrupts all Dio's Best Friends.</c>",
+            destination = "",
+            date        = "",
+            story       = "",
         },
 
         lostSeersLenses = {
             name        = "Lost Seer's Lenses",
             pickup      = "Gain a 0.5% chance to instantly kill a non-boss enemy. <p>Corrupts all Lens-Maker's Glasses.</c>",
             description = "Your attacks have a <y>0.5%</c> <c_stack>(+0.5% per stack)</c> chance to <y>instantly kill</c> a <y>non-Boss enemy</c>. <p>Corrupts all Lens-Maker's Glasses.</c>",
+            destination = "",
+            date        = "",
+            story       = "",
         },
 
         benthicBloom = {
             name        = "Benthic Bloom",
             pickup      = "Upgrades your items at the start of each stage. <p>Corrupts all 57 Leaf Clovers.</c>",
-            description = "<b>Upgrades 3</c> <c_stack>(+3 per stack)</c> random items to items of the next <b>higher rarity</c> at the <b>start of each stage</c>. <p>Corrupts all 57 Leaf Clovers.</c>",
+            description = "<b>Upgrade 3</c> <c_stack>(+3 per stack)</c> random items to items of the next <b>higher rarity</c> at the <b>start of each stage</c>. <p>Corrupts all 57 Leaf Clovers.</c>",
+            destination = "",
+            date        = "",
+            story       = "",
         },
     }
 }
