dee85274bf29c147929205490a48c27313069a3f
Klehrik <78520710+Klehrik@users.noreply.github.com>
2026-03-29 00:36:40 -0400

    ror2 log entries (without date/dest)

--- language/english.lua
+++ language/english.lua
@@ -6,7 +6,7 @@ return {
             description = "Upon hitting an enemy at <y>full health</c>, spawn a <y>lava pillar</c> in a <y>12m</c> <c_stack>(+2.4m per stack)</c> radius for <y>260%</c> <c_stack>(+156% per stack) <y>damage</c>. <p>Corrupts all Will-o'-the-wisps.</c>",
             destination = "",
             date        = "",
-            story       = "",
+            story       = "======================================== \n====   MyBabel Machine Translator   ==== \n====     [Version 12.45.1.009 ]   ====== \n======================================== \nTraining... <100000000 cycles> \nTraining... <100000000 cycles> \nTraining... <100000000 cycles> \nTraining... <102515 cycles> \nComplete! \nDisplay result? Y/N \nY \n======================================== \n\nWe take kindling and throw it in the fire. \n\nThe fire is pleased. The fire grows. \n\nWe take fruit and throw it in the fire. \n\nThe fire loves this gift. Embers pop from the fire and perform dances of joy before fading. Compounds inside the fruit are undone, returning to their base components. There are so many colors. So much energy. \n\nWe take a body and throw it into the fire. It will not be missed. He has more to spare. \n\nThe fire loves this gift. Fat in the body burns, producing a rainbow of beautiful hues in the flame. Muscles contract and snap, stimulated by the gnawing heat. The fire claims its meal, and returns it to energy. \n\nSo much energy. I believe I can work with this.",
         },
 
         weepingFungus = {
@@ -15,7 +15,7 @@ return {
             description = "<g>Heal</c> for <g>2%</c> <c_stack>(+2% per stack) health</c> every second <b>while moving at full speed</c>. <p>Corrupts all Bustling Fungi</c>.",
             destination = "",
             date        = "",
-            story       = "",
+            story       = "<c_stack>//--AUTO-TRANSCRIPTION FROM UES [Redacted] --//</c> \n\n\"This sucks.\" \n\n\"You know, you could have something to eat if you just said the magic word...\" \n\n\"No! I'm not having one of those $#@!ing mushrooms!\" \n\n\"C'mon, they're harmless. In fact, better than harmless, they're--\" \n\n\"Don't even start. And we don't even know that, remember how you got an infection after you first tried them!?\" \n\n\"Common head cold.\" \n\n\"Whatever. My point being, those things probably changed your digestive tract so you could eat them, or some weird $#!& like that. I'm not stooping to your level!\" \n\n\"Well, you're more than welcome to ride your high road with THEM, Mr. Picky Eater. Or, you could stay in the safety of this little cave, and share a snack with me.\" \n\n\"I... I'd rather--\" \n\nROAAAAAAAAAAAAAR!! \n\n\"...Oh my god, fine. Hand it over.\"",
         },
 
         needletick = {
@@ -24,7 +24,7 @@ return {
             description = "<y>10%</c> <c_stack>(+10% per stack)</c> chance to <y>collapse</c> an enemy for <y>400% damage</c>. <p>Corrupts all Tri-Tip Daggers.</c>",
             destination = "",
             date        = "",
-            story       = "",
+            story       = "<c_stack>Welcome to DataScraper (v3.1.53 - beta branch) \n$ Scraping memory... done. \n$ Resolving... done. \n$ Combing for relevant data... done. \nComplete</c> \n\nThe following has been flagged as evidence for use in the trial of Titan v. Cooper, to be presided over by the Supreme Court of Titan. Some details may be expunged for the safety and confidentiality of included persons. \n\n--- \n\n\"Ah... |||||, my dear, do you watch any cartoons?\" \n\n\"P-Please... I w-w-want to go h-h-home...\" \n\n\"You see, there's this little trick they do that I've been meaning to try. You'll help me, won't you...?\" \n\n\"Oh, ||||||||... help me...\" \n\n\"Wonderful. Now stay nice and still for me. Let's see if this works...\" \n\n\"W-What are you... Uh...\" \n\n\"Give it a moment, |||||, my dear. Let me just put this darling toy back in its scabbard.\" \n\n\"W-What are you-- OH GOD!!! HELP ME!!! PLEASE STOP, MY ORGANS--\" \n\n\"Hey, no more tears. I just did something very cool. Let's see if I can do it with our favorite knife, hm?\" \n\n\"PLEASE, NO, PLEASE PLEASE PLEASE\" \n\nThe remainder of this transcript has been sealed. \nReason: vomit in buckets",
         },
 
         polylute = {
@@ -33,7 +33,7 @@ return {
             description = "<y>25%</c> chance to fire <y>lightning</c> for <y>3x60% <c_stack>(+60% per stack) <y>TOTAL damage</c>. <p>Corrupts all Ukuleles.</c>",
             destination = "",
             date        = "",
-            story       = "",
+            story       = "<c_stack>Welcome to DataScraper (v3.1.53 - beta branch) \n$ Scraping memory... done. \n$ Resolving... done. \n$ Combing for relevant data... done. \nComplete</c> \n\nI've made a fas---ating discove-y! Deep in this strange -----, I found some carvings on the c--ffs---. It appears to be... music! There's n----ng else -- could be! I'll att-mp- to t-ansc-be it, thou-- the i-terf--nc- may --ddy -- transmissions. Ahem... \n\nd, d, d, a, G. A-e --- g-t-ing this? Th---'s m--e. \n\n---- --- - - -- - -------- - - - - - ----- - - -- - - - -- - - - \n\nHow catchy!",
         },
 
         saferSpaces = {
@@ -51,7 +51,7 @@ return {
             description = "Gain a <b>10% health shield</c>. While you have shield, hitting an enemy <y>fires a missile</c> that deals <y>40% <c_stack>(+40% per stack) <y>TOTAL damage</c>. <p>Corrupts all AtG Missile Mk. 1s.</c>",
             destination = "",
             date        = "",
-            story       = "",
+            story       = "\"I believe there is a benefit to attacking from behind an impenetrable defense. Some may call it cheating, they may call you a coward, even... I call them targets. History is written by the winners, after all.\" \n\n- Lost Journal, recovered from Petrichor V",
         },
 
         lysateCell = {
@@ -60,7 +60,7 @@ return {
             description = "Add <b>+1 <c_stack>(+1 per stack)</c> charge of your <b>Special skill</c>. <b>Reduces Special skill cooldown</c> by <b>33%</c>. <p>Corrupts all Fuel Cells.</c>",
             destination = "",
             date        = "",
-            story       = "",
+            story       = "\"Recycling is an important thing. And I don't mean recycling a glass bottle to help the environment - though that's important too! No, by 'recycling,' I mean taking one thing, and using it for another. For example, my Ukelele. I love this thing. It has electric properties, and passes them on to my attacks - free chain lightning! But what if I were to 'recycle' it? I think the Uke would make a pretty good melee weapon. Imagine, bludgeoning a Lemurian and pumping it full of volts, in one easy step. \n\nDo you get what I'm saying? There's opportunities for recycling all over the place. You just need to open your mind to the possibilities, and see... And do a little recycling. It's good for the environment.\" \n\n- Lost Journal, recovered from Petrichor V",
         },
 
         pluripotentLarva = {
@@ -69,7 +69,7 @@ return {
             description = "Taking <r>fatal damage</c> <b>consumes this item</c> and revives you with <g>3 seconds of invulnerability</c>, and all of your items that can be <b>corrupted</c> will be. <p>Corrupts all Dio's Best Friends.</c>",
             destination = "",
             date        = "",
-            story       = "",
+            story       = "\"Life finds a way. Even in the depths of creation, there's bound to be something crawling along the sand, just hoping to make it through the day. \n\nWhy don't you take a ride in their shoes, just for one day? You may come out of the experience with a brand new perspective.\"",
         },
 
         lostSeersLenses = {
@@ -78,7 +78,7 @@ return {
             description = "Your attacks have a <y>0.5% <c_stack>(+0.5% per stack) <y>chance</c> to <y>instantly kill</c> a <y>non-Boss enemy</c>. <p>Corrupts all Lens-Maker's Glasses.</c>",
             destination = "",
             date        = "",
-            story       = "",
+            story       = "\"What does it mean to see? \n\nTo a beast, it means to identify potential food and mates. To propagate, to survive. Sight is nothing more than a means to an end, is it not? \n\nTo someone like me... To those who know true purpose... To see is to LIVE. To see is to spot that one little crack in the enemy's armor, to spot that smallest moment of hesitation when they rear back to strike. \n\nTo see the way flesh folds once the skull gives in to your weapon. \n\nYes, it's the little things, but seeing those things... REALLY seeing them. Ah, it fulfills you. I do hope you understand... And if not, you will eventually, if you've read this note.\" \n\n- Lost Journal, recovered from Petrichor V",
         },
 
         benthicBloom = {
@@ -87,7 +87,7 @@ return {
             description = "<b>Upgrade 3</c> <c_stack>(+3 per stack)</c> random items to items of the next <b>higher rarity</c> at the <b>start of each stage</c>. <p>Corrupts all 57 Leaf Clovers.</c>",
             destination = "",
             date        = "",
-            story       = "",
+            story       = "\"Luck? Who needs luck, in a place like this. \n\nTo survive here, you'd need more than just luck. You need intuition. Instinct. The right tools. Power. \n\nI know you're scared. And I am too. But if there's anything this hell hole has taught me... \n\nWe make our own luck.\" \n\n- Signal Echoes, UES Contact Light",
         },
     }
 }
